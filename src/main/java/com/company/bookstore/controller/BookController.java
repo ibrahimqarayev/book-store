cc8e974ede12f1eeadc8b7f104c2d0ba36699cd7
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-01-29 20:46:37 +0400

    Added BookController

--- src/main/java/com/company/bookstore/controller/BookController.java
+++ src/main/java/com/company/bookstore/controller/BookController.java
@@ -1,4 +1,20 @@
 package com.company.bookstore.controller;
 
+import com.company.bookstore.service.BookService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class BookController {
+    private final BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+
 }
