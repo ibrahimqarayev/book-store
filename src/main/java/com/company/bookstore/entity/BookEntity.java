1f49138e7d175836702f930c32fd56b3134f1fbd
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-01-29 20:47:06 +0400

    Added BookEntity

--- src/main/java/com/company/bookstore/entity/BookEntity.java
+++ src/main/java/com/company/bookstore/entity/BookEntity.java
@@ -1,4 +1,67 @@
 package com.company.bookstore.entity;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "book")
 public class BookEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String author;
+    private String title;
+    private Double price;
+
+    public BookEntity() {
+    }
+
+    public BookEntity(Integer id, String name, String author, String title, Double price) {
+        this.id = id;
+        this.name = name;
+        this.author = author;
+        this.title = title;
+        this.price = price;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
 }
