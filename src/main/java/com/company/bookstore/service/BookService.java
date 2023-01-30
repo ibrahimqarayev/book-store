62058aa66de9f8aecac26496d606642732890cef
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-01-30 19:46:36 +0400

    Added BookService

--- src/main/java/com/company/bookstore/service/BookService.java
+++ src/main/java/com/company/bookstore/service/BookService.java
@@ -1,14 +1,31 @@
 package com.company.bookstore.service;
 
+import com.company.bookstore.converter.BookConverter;
+import com.company.bookstore.dto.BookDto;
+import com.company.bookstore.entity.BookEntity;
 import com.company.bookstore.repository.BookRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class BookService {
     private final BookRepository bookRepository;
+    private final BookConverter bookConverter;
 
-    public BookService(BookRepository bookRepository) {
+    public BookService(BookRepository bookRepository, BookConverter bookConverter) {
         this.bookRepository = bookRepository;
+        this.bookConverter = bookConverter;
     }
+
+    public BookDto addBook(BookEntity bookEntity) {
+        return bookConverter.toBookDto(bookEntity);
+    }
+
+    public List<BookDto> getAllBooks() {
+        return bookRepository.findAll().stream().map(bookEntity -> bookConverter.toBookDto(bookEntity)).collect(Collectors.toList());
+    }
+
 
 }
