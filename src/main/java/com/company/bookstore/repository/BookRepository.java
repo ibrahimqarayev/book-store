64bc303244ea70aac6dcf495db4ea3e0bf06c599
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-02-01 23:22:45 +0400

    Added BookRepository

--- src/main/java/com/company/bookstore/repository/BookRepository.java
+++ src/main/java/com/company/bookstore/repository/BookRepository.java
@@ -1,10 +1,12 @@
 package com.company.bookstore.repository;
 
+import com.company.bookstore.dto.BookDto;
 import com.company.bookstore.entity.BookEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface BookRepository extends JpaRepository<BookEntity, Integer> {
-    Optional<BookEntity> findByName(String name);
+    Optional<List<BookDto>> findByName(String name);
 }
