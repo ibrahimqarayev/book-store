40623866c9ed1afd0764fc41a88b1afa4252771f
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-02-02 22:31:10 +0400

    Added BookController

--- src/main/java/com/company/bookstore/controller/BookController.java
+++ src/main/java/com/company/bookstore/controller/BookController.java
@@ -1,11 +1,16 @@
 package com.company.bookstore.controller;
 
+import com.company.bookstore.converter.BookConverter;
 import com.company.bookstore.dto.BookDto;
 import com.company.bookstore.entity.BookEntity;
+import com.company.bookstore.entity.MyBooks;
 import com.company.bookstore.service.BookService;
+import com.company.bookstore.service.MyBooksService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -14,9 +19,13 @@ import java.util.List;
 @Controller
 public class BookController {
     private final BookService bookService;
+    private final MyBooksService myBooksService;
+    private final BookConverter bookConverter;
 
-    public BookController(BookService bookService) {
+    public BookController(BookService bookService, MyBooksService myBooksService, BookConverter bookConverter) {
         this.bookService = bookService;
+        this.myBooksService = myBooksService;
+        this.bookConverter = bookConverter;
     }
 
     @GetMapping("/index")
@@ -25,19 +34,15 @@ public class BookController {
     }
 
     @GetMapping("/books")
-    public ModelAndView getAllBooks() {
-        List<BookDto> list = bookService.getAllBooks();
-//        ModelAndView modelAndView = new ModelAndView();
-//        modelAndView.setViewName("books");
-//        modelAndView.addObject("book", list);
-//        return modelAndView;
-        return new ModelAndView("books", "book", list);
+    public String getAllBooks() {
+        return "books";
     }
 
     @GetMapping("/search")
     public String searchBook() {
         return "search";
     }
+
 
     @GetMapping("/new_releases")
     public String newReleases() {
@@ -69,11 +74,49 @@ public class BookController {
         return "faqs";
     }
 
-    @PostMapping("/add")
-    public String addBook(@ModelAttribute BookEntity bookEntity) {
-        bookService.addBook(bookEntity);
-        return "redirect:/books";
+// -------------
+
+    @GetMapping("/home")
+    public String home() {
+        return "home";
     }
 
+    @GetMapping("/book_register")
+    public String bookRegister() {
+        return "bookRegister";
+    }
+
+    @GetMapping("/available_books")
+    public ModelAndView getAllBook() {
+
+        List<BookDto> list = bookService.getAllBooks();
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("bookList");
+        modelAndView.addObject("book", list);
+        //return new ModelAndView("bookList", "book", list);
+        return modelAndView;
+
+    }
+
+    @PostMapping("/save")
+    public String addBook(@ModelAttribute BookEntity bookEntity) {
+        bookService.save(bookEntity);
+        return "redirect:/available_books";
+    }
+
+    @GetMapping("/my_books")
+    public String getMyBooks(Model model) {
+        List<MyBooks> list = myBooksService.getAllMyBooks();
+        model.addAttribute("book", list);
+        return "myBooks";
+    }
+
+
+    @GetMapping("/myList/{id}")
+    public String getMyBooksById(@PathVariable Integer id) {
+        BookDto bookDto = bookService.getBookById(id);
+        myBooksService.saveMyBooks(bookConverter.toMyBooks(bookDto));
+        return "redirect:/my_books";
+    }
 
 }
