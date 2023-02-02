b5e2ad3b351adbb6a566f2f9552120ee7b196d73
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-02-02 22:35:54 +0400

    Added BookController

--- src/main/java/com/company/bookstore/converter/BookConverter.java
+++ src/main/java/com/company/bookstore/converter/BookConverter.java
@@ -2,27 +2,27 @@ package com.company.bookstore.converter;
 
 import com.company.bookstore.dto.BookDto;
 import com.company.bookstore.entity.BookEntity;
+import com.company.bookstore.entity.MyBooks;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BookConverter {
-
-    public BookEntity toBookEntity(BookDto from) {
-        return new BookEntity(
-                from.getId(),
-                from.getName(),
-                from.getAuthor(),
-                from.getTitle(),
-                from.getPrice()
-        );
-    }
 
     public BookDto toBookDto(BookEntity from) {
         return new BookDto(
                 from.getId(),
                 from.getName(),
                 from.getAuthor(),
-                from.getTitle(),
+                from.getDescription(),
+                from.getPrice()
+        );
+    }
+
+    public MyBooks toMyBooks(BookDto from) {
+        return new MyBooks(
+                from.getId(),
+                from.getName(),
+                from.getAuthor(),
                 from.getPrice()
         );
     }
