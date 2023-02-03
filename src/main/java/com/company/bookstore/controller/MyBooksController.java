9af3b0c6ed0b7101c047b61254f8f027ec8b9b67
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-02-03 23:35:20 +0400

    Added MyBooksController

--- src/main/java/com/company/bookstore/controller/MyBooksController.java
+++ src/main/java/com/company/bookstore/controller/MyBooksController.java
@@ -2,6 +2,8 @@ package com.company.bookstore.controller;
 
 import com.company.bookstore.service.MyBooksService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class MyBooksController {
@@ -9,5 +11,11 @@ public class MyBooksController {
 
     public MyBooksController(MyBooksService myBooksService) {
         this.myBooksService = myBooksService;
+    }
+
+    @DeleteMapping("/deleteMyList/{id}")
+    public String deleteMyBooks(@PathVariable Integer id) {
+        myBooksService.deleteById(id);
+        return "redirect:/my_books";
     }
 }
